2ff33fcc010ea2d8b2fd6c9bebafd17acd0b1677
RodolfoFariia <d2024008886@unifei.edu.br>
2025-06-20 18:56:03 -0300

    definindo structs arvore rb

--- Rubro_Negra/arvRB.c
+++ Rubro_Negra/arvRB.c
@@ -2,3 +2,20 @@
 #include <stdlib.h>
 #include "arvRB.h"
 
+// Definindo struct da árvore rubro negra
+struct arvoreRB{
+    no *sentinelaRaiz;
+    no *sentinelaFolha;
+};
+
+
+// Definindo struct do nó
+struct no {
+    no *pai;
+    no *dir;
+    no *esq;
+    char cor;
+    int chave;
+    int alturaPreto;
+};
+
--- Rubro_Negra/arvRB.h
+++ Rubro_Negra/arvRB.h
@@ -2,5 +2,9 @@
 #ifndef ARVRB_ARVRB_H
 #define ARVRB_ARVRB_H
 
+// renomeando structs
+typedef struct arvoreRB arvRb;
+typedef struct no no;
+
 
 #endif
